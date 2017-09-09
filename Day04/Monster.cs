ba3ed458dc00ba0e15d261a8f67f46bac4cd9bd1
JuniosSTL <junios@naver.com>
2017-09-09 16:47:07 +0900

    클래스 생성 상속 첫번째 구현

---
+++ Day04/Monster.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day04
+{
+    class Monster
+    {
+        public string name;
+        public int hp;
+        public int attack;
+        public float moveSpeed;
+
+        public void Move()
+        {
+            Console.WriteLine(name + " 이동");
+        }
+    }
+}
--- Day04/Program.cs
+++ Day04/Program.cs
@@ -6,7 +6,39 @@ namespace Day04
     {
         static void Main(string[] args)
         {
-            A a = new A();
+#region Player
+            Player[] players = new Player[2];
+            players[0] = new Player();
+            players[0].id = "junios";
+            players[0].name = "강희운";
+            players[0].attack = 100;
+            players[0].hp = 100;
+            players[0].moveSpeed = 10;
+
+            players[1] = new Player();
+            players[1].id = "babo";
+            players[1].name = "김대유";
+            players[1].attack = 10;
+            players[1].hp = 10;
+            players[1].moveSpeed = 0.1f;
+
+            players[0].Move();
+            players[1].Move();
+            #endregion
+
+#region 몬스터
+            Goblin goblin = new Goblin();
+            WildPig pig = new WildPig();
+            Slime slime = new Slime();
+            BlueSlime bSlime = new BlueSlime();
+
+            slime.name = "슬라임";
+            slime.Move();
+
+            bSlime.name = "파란슬라임";
+            bSlime.Move();
+#endregion
+
         }
     }
 }
