be1ec4461434c2ce825a9304845d1692b8a68eda
JuniosSTL <junios@naver.com>
2017-09-09 15:31:53 +0900

    데이터 모델링 자료

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -6,47 +6,7 @@ namespace Day04
     {
         static void Main(string[] args)
         {
-            int a = new int(); // ==> int a; //legacy
-            float b = new float(); // == flat b;
-
-            Program p = new Program();
-            p.Add();
-
-            A.Display();
-            A.display = 100;
+            A a = new A();
         }
-
-        void Add()
-        {
-
-        }
-    }
-
-    class A
-    {
-        public static void Display()
-        {
-            Console.WriteLine("Class A");
-        }
-
-        public static int display;
-    }
-
-    class B
-    {
-        void Test()
-        {
-            A.display = 20;
-        }
-
-
-        public static void Display()
-        {
-            Console.WriteLine("Class B");
-        }
-    }
-
-    class C
-    {
     }
 }
